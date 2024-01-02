2995ee4764d5a68fee707bce240577626f3dda26
Egbe Timmy <88975749+Egbetimmy@users.noreply.github.com>
2024-01-02 17:38:45 +0100

    updated

--- Assignment/Models/Profile.cs
+++ Assignment/Models/Profile.cs
@@ -14,5 +14,30 @@ namespace Assignment.Models
         public string Phone { get; set; }
         public string Photo { get; set; }
         public string LocalGovt { get; set; }
+        public Users User { get; set; }
     }
 }
+
+/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Photo { get; set; }
+        public string LocalGovt { get; set; }
+
+        // Reference to the User
+        public User User { get; set; }
+    }
+}
+
+ */
